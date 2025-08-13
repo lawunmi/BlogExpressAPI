956d40cb63e6910a25ce41ef0949dae97c9d8e99
Mistura Raheem <olawunmiraheem@gmail.com>
2025-08-12 23:00:46 -0400

    Project setup

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+// Middleware
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ message: "Health check OK" });
+});
+
+// Swagger setup
+const swaggerDocument = YAML.load("src/swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
